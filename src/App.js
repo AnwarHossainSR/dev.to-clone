ef4e43fae5ee7e300e682e157ad921fd483f8767
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-23 11:39:18 +0600

    connect with component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "./App.scss";
+import Content from "./components/Content";
+import LeftSidebar from "./components/LeftSidebar";
+import Navigation from "./components/Navigation";
+import RightSidebar from "./components/RIghtSidebar";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navigation />
+      <main className="main-container">
+        <LeftSidebar />
+        <Content />
+        <RightSidebar />
+      </main>
+    </>
+  );
 };
 
 export default App;
