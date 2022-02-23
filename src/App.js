e18345675deb07dfd92f6d824f6e92929d833aa8
AnwarHossainSR <mahedianwar@gmail.com>
2022-02-23 14:57:23 +0600

    Right sidebar Design

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.scss";
 import Content from "./components/Content";
 import LeftSidebar from "./components/LeftSidebar";
 import Navigation from "./components/Navigation";
-import RightSidebar from "./components/RIghtSidebar";
+import RightSidebar from "./components/RightSidebar";
 
 const App = () => {
   return (
