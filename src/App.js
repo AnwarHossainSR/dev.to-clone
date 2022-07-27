5a38fdd0f0d9488575b14b2f7b39ccefb23ef410
AnwarHossainSR <mahedianwar@gmail.com>
2022-07-27 20:39:53 +0600

    update file name

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.scss";
 import Content from "./components/Content";
 import LeftSidebar from "./components/LeftSidebar";
 import Navigation from "./components/Navigation";
-import RightSidebar from "./components/RightSidebar";
+import RightSidebar from "./components/RightSidebar.jsx";
 
 const App = () => {
   return (
